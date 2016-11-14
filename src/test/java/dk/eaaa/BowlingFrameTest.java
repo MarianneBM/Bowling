2663d22a88829f1d9683deb81a7a65e9263fddf1
Marianne <Marianne>
2016-11-14 11:18:13 +0100

    nyt 4 checkin

--- src/test/java/dk/eaaa/BowlingFrameTest.java
+++ src/test/java/dk/eaaa/BowlingFrameTest.java
@@ -1,7 +1,7 @@
 package dk.eaaa;
 
 import static org.junit.Assert.*;
-
+//
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,7 +16,7 @@ public class BowlingFrameTest {
 	
 	@Test
 	public void testGetScore() throws Exception {
-		assertEquals(10, frame.getScore());
+		assertEquals(0, frame.getScore());
 		frame.rollFirst(5);
 		assertEquals(5, frame.getScore());
 		frame.rollSecond(1);
